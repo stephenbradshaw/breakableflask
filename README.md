2d073150162c2fe82fdb6a0f537e91eaa4d8947c
stephenbradshaw <stephen.bradshaw@digital.gov.au>
2017-02-16 10:35:50 +1100

    First commit

---
+++ README.md
@@ -0,0 +1,16 @@
+Breakable Flask
+
+
+A simple vulnerable Flask application.
+
+This can be used to test out and learn exploitation of common web application vulnerabilities. 
+
+Originally written because I wanted a very simple, single page vulnerable app that I could quickly run up to perform exploitation checks against. 
+
+A the moment, the following vulnerabilities are present:
+* Python code injection
+* Operating System command injection
+* Python deserialisation of arbitrary data (pickle)
+
+
+New vulnerabilities may be added from time to time as I have need of them.
---
+++ breakableflask.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+
+import ssl
+import os
+import pickle
+from base64 import b64decode,b64encode
+from os import popen
+
+from flask import Flask, request, make_response
+
+
+app = Flask(__name__)
+
+
+def rp(command):
+    return popen(command).read()
+
+
+@app.route('/')
+def index():
+    return """
+    <html>
+    <head><title>Vulnerable Flask App</title></head>
+    <body>
+        <p><h3>Functions</h3></p>
+        <a href="/cookie">Set and get cookie value</a><br>
+        <a href="/lookup">Do DNS lookup on address</a><br>
+        <a href="/evaluate">Evaluate expression</a><br>
+    </body>
+    </html>
+    """
+
+
+# code injection
+@app.route('/evaluate', methods = ['POST', 'GET'])
+def evaluate():
+    expression = None
+    if request.method == 'POST':
+        expression = request.form['expression']
+    return """
+    <html>
+       <body>""" + "Result: " + (str(eval(expression)).replace('\n', '\n<br>')  if expression else "") + """
+          <form action = "/evaluate" method = "POST">
+             <p><h3>Enter expression to evaluate</h3></p>
+             <p><input type = 'text' name = 'expression'/></p>
+             <p><input type = 'submit' value = 'Evaluate'/></p>
+          </form>
+       </body>
+    </html>
+    """
+
+
+
+# os command injection
+@app.route('/lookup', methods = ['POST', 'GET'])
+def lookup():
+    address = None
+    if request.method == 'POST':
+        address = request.form['address']
+    return """
+    <html>
+       <body>""" + "Result:\n<br>\n" + (rp("nslookup " + address).replace('\n', '\n<br>')  if address else "") + """
+          <form action = "/lookup" method = "POST">
+             <p><h3>Enter address to lookup</h3></p>
+             <p><input type = 'text' name = 'address'/></p>
+             <p><input type = 'submit' value = 'Lookup'/></p>
+          </form>
+       </body>
+    </html>
+    """
+
+    
+
+# deserialisation vulnerability
+@app.route('/cookie', methods = ['POST', 'GET'])
+def cookie():
+    cookieValue = None
+    value = None
+    
+    if request.method == 'POST':
+        cookieValue = request.form['value']
+        value = cookieValue
+    elif 'value' in request.cookies:
+        cookieValue = pickle.loads(b64decode(request.cookies['value'])) 
+    
+        
+    form = """
+    <html>
+       <body>Cookie value: """ + str(cookieValue) +"""
+          <form action = "/cookie" method = "POST">
+             <p><h3>Enter value to be stored in cookie</h3></p>
+             <p><input type = 'text' name = 'value'/></p>
+             <p><input type = 'submit' value = 'Set Cookie'/></p>
+          </form>
+       </body>
+    </html>
+    """
+    resp = make_response(form)
+    
+    if value:
+        resp.set_cookie('value', b64encode(pickle.dumps(value)))
+
+    return resp
+
+
+app.run('localhost', 4000, app)
+
